eb90a7ea5cd7f2d991f7b618d8845053d0ffae1c
Mustafa Paksoy <paksoy@gmail.com>
2011-08-31 15:01:23 -0700

    Remove test code.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,8 +26,6 @@ public class Application extends Controller {
     }
 
     public static void index() throws Exception {
-        if (1 < 2)
-        throw new IllegalStateException(new RuntimeException("inside"));
     	User user = RequiresLogin.getActiveUser();
     	
     	if(user.getLocationStr() == null){
